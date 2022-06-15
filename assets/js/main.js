bd58ae010e806bbc2690bddab9395dab29338270
Irina403 <irinat403@gmail.com>
2022-06-15 16:23:27 +0300

    15.06.23 16:23 end

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,9 +6,18 @@ $(".advantages__button").click(function() {
 	$(".advantages__item").toggleClass("active");
 });
 
-
-
-
+$(document).ready(()=> {
+	$('.slick-slider').slick({
+		arrows:true,
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		appendArrows: $('.your-class-arrow'),
+		prevArrow: '<img class="slick-arrow prev" src="../i/icons/icon-prev.svg" alt="" " aria-hidden="true"></img>',
+		nextArrow: '<img class="slick-arrow next" src="../i/icons/icon-next.svg" alt="" " aria-hidden="true"></img>'
+		
+	});
+})
 
 
 
